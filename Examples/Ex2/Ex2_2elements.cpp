ac4a60a8380f9fefb5b0b6e0033919baa37e87b1
luchouba <luciano.buglioni@gmail.com>
2020-02-26 14:01:10 -0300

    begining to add example 2 output file

--- Examples/Ex2/Ex2_2elements.cpp
+++ Examples/Ex2/Ex2_2elements.cpp
@@ -40,7 +40,8 @@ class outfile
 
 	public:
 	//Constructors
-	outfile(string name, FeGrid &grid<2>);
+	template <int dim>
+	outfile(string name, FeGrid &grid<dim>);
 
 }
 	
@@ -174,83 +175,12 @@ int main()
 	
 	logfile.close();
 	
-	outfile of("output.vtk",grid);
-	
 	logfile << "Stiffness Matrix \n\n";
 	logfile << Kel.outstr();
 
 	
 	logfile.close();
 	return 0;
-}
 
-
-outfile::outfile(string name, FeGrid &grid<2>)
-{
-	int Rank=0;
-	
-	// string fileName=name;
-	// ofstream file;
-	// file.open((fileName).c_str(),ios::out);
-	// file << "<?xml version=\"1.0\"?>" << endl;
-	// file << "<VTKFile type=\"UnstructuredGrid\">" << endl;
-	// file << "<UnstructuredGrid>" << endl;
-	// file << "<Piece NumberOfPoints=\"" << grid.Num_Verts() << "\" NumberOfCells=\"" << grid.Num_Cells() << "\">" << endl;
-	// file << "<Points>" << endl;
-	// file << "<DataArray NumberOfComponents=\"3\" type=\"Float32\" format=\"ascii\" >" << endl;
-	// for (int n=0;n<grid.Num_Verts();++n) {
-		// for (int i=0; i<3; ++i) file<< setw(16) << setprecision(8) << scientific << grid.Node(n).Comp()[i] << endl;
-	// }
-	// file << "</DataArray>" << endl;
-	// file << "</Points>" << endl;
-	// file << "<Cells>" << endl;
-	
-	// file << "<DataArray Name=\"connectivity\" type=\"Int32\" format=\"ascii\" >" << endl;
-	// for (int c=0;c<grid.Num_Cells();++c) {
-		// for (int n=0;n<grid.Cell(c).Num_Vertex();++n) {
-			// file << grid.Cell(c).Vert(n) << "\t";}
-		// file << endl;}
-	
-	// file << "</DataArray>" << endl;
-	// file << "<DataArray Name=\"offsets\" type=\"Int32\" format=\"ascii\" >" << endl;
-	// int offset=0;
-	// for (int c=0;c<grid.Num_Cells();++c) {
-		// offset+=grid.Cell(c).Num_Vertex();
-		// file << offset << endl;}
-	// file << "</DataArray>" << endl;
-	
-	// file << "<DataArray Name=\"types\" type=\"UInt8\" format=\"ascii\" >" << endl;
-	// for (int c=0;c<grid.Num_Cells();++c) {
-		// if (grid.Cell(c).Num_Vertex()==4) file << "10" << endl; // Tetra
-		// if (grid.Cell(c).Num_Vertex()==8) file << "12" << endl; // Hexa
-		// if (grid.Cell(c).Num_Vertex()==6) file << "13" << endl; // Prism
-		// if (grid.Cell(c).Num_Vertex()==5) file << "14" << endl; // Pyramid (Wedge)
-	// }
-	// file << endl;
-	// file << "</DataArray>" << endl;;
-	
-	// file << "</Cells>" << endl;
-	
-	// file << "<CellData Scalars=\"scalars\" format=\"ascii\">" << endl;
-	
-	// //Begin data field output
-	// file << "<DataArray Name=\"";
-
-	// file << "Var";
-	// file << "\" type=\"Float32\" format=\"ascii\" >" << endl;
-
-	// //If scalars
-	// for (int n=0;n<grid.Num_Nodes();n++)
-		// file << field.Val(n).Comp()[0] << endl;
-
-	// file << "</DataArray>" << endl;
-
-	// // End of data output
-	// file << "</CellData>" << endl;
-	
-	// file << "</Piece>" << endl;
-	// file << "</UnstructuredGrid>" << endl;
-	// file << "</VTKFile>" << endl;
-	// file.close();
 
 }
