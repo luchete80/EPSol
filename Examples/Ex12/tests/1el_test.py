21e5d1b4f2c9233baf2505630ee21e794baecd3f
lucho-nb <luciano.buglioni@gmail.com>
2020-12-22 15:03:58 -0300

    working on new 1 element example

--- Examples/Ex12/tests/1el_test.py
+++ Examples/Ex12/tests/1el_test.py
@@ -5,41 +5,19 @@ from numpy import *
 import numpy.matlib #for zeros
 
 import array as arr
-import deriv
 
 #******************************************************
 #----------------------------
 #Input Data------------------
 lx=1.
-ly=20.*3.1415926/180.
+ly=1
 nex=1
 ney=1
-numit=1
-
-#RADIAL FLOW EXAMPLE 
-r0=1.
-r1=2.
+numit=200 
 
 #-------------
 numvars=1 #1: Only F tensor, 2: F and internal variable s
-#---------------------------------------------------
-#Material properties (Table 2.1 p28)
-#HSLA-65 steel with strain rate between e-3 and e-4
-#---------------------------------------------------
 
-mat_A0 =6.34e11
-mat_psi=3.25
-mat_m  =0.1956
-mat_n  =0.06869
-mat_s0 =80.0e6
-mat_Q  =312.35e3
-mat_R  =8.314
-mat_a  =1.5
-mat_h0 =3093.1e6
-mat_s0 =125.1e6
-
-ey=1.e9
-nu=0.499
 ##********************** INPUT END 
 
 #***** MESH**********
@@ -72,23 +50,13 @@ for nx in range (ney+1):
 #BEFORE CONVERT TO CARTESIAN!
 #Only for this example   
 print("************VELOCITIES***************")
-for n in range(numnodes):
-    r=node[n,0]+r0
-    t=node[n,1]-ly/2.
-    vr=0.1*r0/r
-    vnxy[n,0]=vr*cos(t)
-    vnxy[n,1]=vr*sin(t)
+vnxy[0,0]=0.;vnxy[0,1]=1.
+vnxy[1,0]=0.1;vnxy[1,1]=1.
+vnxy[2,0]=0.;vnxy[2,1]=1.
+vnxy[3,0]=0.1;vnxy[3,1]=1.
+
+for n in range (numnodes):
     print("vxy ",n,":",vnxy[n,0],vnxy[n,1])
-
-
-#Radial flow example: convert from cilindrical to cartesian
-for n in range(numnodes):
-   r=node[n,0]+r0
-   t=node[n,1]-ly/2.
-   node[n,0]=r*cos(t)
-   node[n,1]=r*sin(t)
-   print("Coord ",n,":",node[n,0],node[n,1])
-   
  
 print(node)
 #Connectivity
@@ -128,9 +96,7 @@ X2=numpy.matlib.zeros((4, 2))
 #Nodal values
 U   =matrix(numpy.matlib.zeros((44, 1)))
 UV  =matrix(numpy.matlib.zeros((8, 1)))
-Usig=matrix(numpy.matlib.zeros((16, 1)))
 UF  =matrix(numpy.matlib.zeros((16, 1)))
-UFvp=matrix(numpy.matlib.zeros((20, 1)))
 Us  =matrix(numpy.matlib.zeros((4, 1)))
 
 #Gauss variables
@@ -144,7 +110,6 @@ v=matrix(numpy.matlib.zeros((2, 1)))    #Gauss Point velocity
 Ns=matrix(numpy.matlib.zeros((1, 4)))
 Nv=matrix(numpy.matlib.zeros((2, 8)))
 NsigF=matrix(numpy.matlib.zeros((4, 16)))
-NFvp=matrix(numpy.matlib.zeros((5, 20)))
 
 Dvp=matrix(numpy.matlib.zeros((4, 1)))      #From plastic deformation direction
 DM =matrix(numpy.matlib.zeros((5, 5)))   #4.24
@@ -156,37 +121,21 @@ Bs=matrix(numpy.matlib.zeros((2, 8)))
 Bv=matrix(numpy.matlib.zeros((4, 8)))
 #BsigF=[matrix(numpy.matlib.zeros((4, 16))),matrix(numpy.matlib.zeros((4, 8)))]
 BsigF=arange(128).reshape(4,16,2) #
-BFvp =arange(200).reshape(5,20,2) #
 
 temp4x16=matrix(numpy.matlib.zeros((4, 16)))
 
-#(5,20,2) x (20)
-BUFvp=matrix(numpy.matlib.zeros((5, 2)))
-
 B4i=arange(32).reshape(4,4,2) #
-B5i=arange(50).reshape(5,5,2) #
 #(4,16,2)
 #print(BsigF[0])
 LM =matrix(numpy.matlib.zeros((4, 4)))
-LM5=matrix(numpy.matlib.zeros((5, 5)))
 
-
-K=matrix(numpy.matlib.zeros((44, 44)))
 
 R   =[  matrix(numpy.matlib.zeros((16, 1))),
         matrix(numpy.matlib.zeros(( 4, 1)))]
 RF  =matrix(numpy.matlib.zeros((16, 1)))
 Rsig=matrix(numpy.matlib.zeros((16, 1)))
-RFvp=matrix(numpy.matlib.zeros((20, 1)))
 Rs  =matrix(numpy.matlib.zeros((4, 1)))
 Rv  =matrix(numpy.matlib.zeros((8, 1)))
-
-#Formulation 1
-#-----------------------------------
-#Symmetric tensors
-Ee =matrix(numpy.matlib.zeros((4, 1)))
-Eet=matrix(numpy.matlib.zeros((2, 2))) #Tensor form 
-
 
 
 #These are the same but reorganized
@@ -195,11 +144,6 @@ L   =matrix(numpy.matlib.zeros((2, 2)))
 BL      = arange(128).reshape(4,4,8)            #Eqns 2.33, B.17
 temp8x1 = matrix(numpy.matlib.zeros((8, 1))) 
 
-
-#Stress
-sig  =matrix(numpy.matlib.zeros((4, 1)))    #Stress Gauss Points
-sig_d=matrix(numpy.matlib.zeros((4, 1)))    #Deviatoric, is also symmetric
-N_d  =matrix(numpy.matlib.zeros((4, 1)))    #Direction of plastic strain
 
 P=matrix(numpy.matlib.zeros((4, 1))) 
 
@@ -215,52 +159,11 @@ class bMatrix: #Block matrix
     def __init__(self,i,j):
         m=matrix(numpy.matlib.zeros((i, j)))
  
-#Matrix Double Entry
-#Example in formulation 1:
-# ( 8x 8)  (8x16)  (8x20) (8x4)
-# (16x 8) (16 x 16) ..  ..
-# (20x 8) (20x16) (20x20) (20x4)
-# ( 4x 8)
-
-dDdUv   =arange(200).reshape(5,5, 8) #TO MODIFY
-dDdUF   =arange(400).reshape(5,5,16) #TO MODIFY
-dDdUFvp =arange(500).reshape(5,5,20) #TO MODIFY
-dDdUs   =arange(100).reshape(5,5, 4) #TO MODIFY
-
-#From multiplying dDdU x NFvp*
-#4.39 to 4.42
-temp_dDFvp=[numpy.matlib.zeros((5, 8)),
-            numpy.matlib.zeros((5,16)),
-            numpy.matlib.zeros((5,20)),
-            numpy.matlib.zeros((5, 4))]
-
 Kt=[
      [matrix(numpy.matlib.zeros(( var_edof.astype(int)[0], var_edof.astype(int)[0]))), matrix(numpy.matlib.zeros((var_edof.astype(int)[0], var_edof.astype(int)[1])))]
      ,
      [matrix(numpy.matlib.zeros(( var_edof.astype(int)[1], var_edof.astype(int)[0]))), matrix(numpy.matlib.zeros((var_edof.astype(int)[1], var_edof.astype(int)[1])))]
     ] 
-print ("Kt", Kt)
-    
-dgdU=[  matrix(numpy.matlib.zeros((1, 8))),
-        matrix(numpy.matlib.zeros((1,16))),
-        matrix(numpy.matlib.zeros((1,20))),
-        matrix(numpy.matlib.zeros((1, 4)))]
-
-print("Kt_0",len((Kt[1][0])),len((Kt[1][0]).transpose()))
-
-ck = ey/ ((1. + nu)*(1. - 2. * nu))
-c=matrix(numpy.matlib.zeros((4, 4)))
-c[0,0]=c[1,1]=c[2,2]=ck*(1.-nu)                 
-c[0,1]=c[1,0]=c[0,2]=c[2,0]=c[2,1]=c[1,2]=ck*nu
-c[3,3]=ck*(.5 - nu)
-print ("C matrix")
-print(c)
-#Set G and H matrices
-#2.32
-G=matrix([[1,0,0,0],[0,0,0,1],[0,0,0,1],[0,1,0,0]]) 
-H=matrix([[1,0,0,0],[0,1,0,0],[0,0,0,0],[0,0,0,0]]) 
-
-
 #---------------------------------------------------
 #Before start, assign Uglob values
 #---------------------------------------------------
@@ -360,13 +263,15 @@ while (it < numit):
                     juf+=var_dim[0]
                     iv=iv+2
 
-                #print("UV",UV)
-                #print("UF",UF)
+                print("UV",UV)
+                print("UF",UF)
                 
                 v  =Nv*UV #[2x8 x (8x1)]
+                print ("v",v)
                 s  =float(Ns*Us)
                 F  =NsigF*UF #[(4x16)*(16x1) =(4x1)]
                 
+                print ("F",F)
                  
                 #Galerkin strain integration
                 #Calculate deformation gradient Fij, for that
@@ -383,6 +288,8 @@ while (it < numit):
                 #Calculate 
                 #Ea ij= 1/2(Lki F(-1)lk F(-1)LJ +F(-1)ki F(-1)KL Llj )
                 dVxy=Bv*UV #(4x8)*(8x1)=(4x1) (vx,x vx,y vy,x vy,y)T 
+                
+                print ("dVxy",dVxy)
                 L[0,0]=dVxy[0]
                 L[0,1]=dVxy[1]
                 L[1,0]=dVxy[2]
@@ -397,11 +304,7 @@ while (it < numit):
                 LM[0,1]=LM[2,3]=dVxy[1]
                 LM[1,0]=LM[3,2]=dVxy[2]
                 LM[1,1]=LM[3,3]=dVxy[3]
-                
-                for i in range(4):
-                    for j in range(4):               
-                        LM5[i,j]=LM[i,j]
-                        
+
                 #BL interpolators BLijk (4,4,8) (B.17 p165)
                 for k in range(8):
                     BL[0,0,k]=BL[2,2,k]=Bv[0,k]
@@ -423,51 +326,7 @@ while (it < numit):
                 
                 #Calculate stabilization parameter
                 tau=1.
-                #STRESSES**********
-                #From 2.27 Plane Strain Symmetric tensors are defined as 
-                #t=[txx tyy tzz tyz]
-                pi=1./3.*(sig[0,0]+sig[1,0]+sig[2,0])
-                for i in range(3): #Only daigonal is modified
-                    sig_d[i,0]=sig[i,0]-pi #comps are [x y z yz]
-                #print ("sigd",sig_d[i][0])   
-                for k in range(4):
-                    sig_eq=sqrt(1.5*(sig_d[k,0]))
-                #*** STRAINS
-                #Equivalent strain rate
-                mat_A=mat_A0*math.exp(-mat_Q/mat_R)
-                #Equivalent viscoplastic strain rate
-                epsr_vp_eq=0.
-                if (s!=0):
-                    epsr_vp_eq=mat_A*(sinh(mat_psi*sig_eq/s))**(1./mat_m)
-                
-                #print (epsr_vp_eq)
-                #Evaluate g function 2.58/4.48
-                g_sigs=1.
-                #g_sigs=h0*|(1-s/s*)|*sign(1-s/s*)A(sinh())
-                #With s*
-                #s*=s~(edot~_vp/A)^n
-                #Direction of plastic Strain: 2.16
-                N_d=sqrt(3./2.)*sig_d/sig_eq
-                Dvp=sqrt(3./2.)*epsr_vp_eq*N_d
-                #print ("Nd",N_d)
-                #With evp = f(sigma,s) (Sec. 2.4.2)
-                #eps_vp=A sinh (psi (sigma/s))^(1/m) (2.57 p27)
-                #Calculate Rate of Def Tensor D (2.13, 2.14)
-                #D(th)ij=alpha vk dT/dxk deltaij 2.13
-                #IN THIS EXAMPLE THIS DTH 0
-                #D(vp)ij=sqrt(3/2) e. vp Nij  2.14
-                #Nij Direction of plastic flow
-                #Assemble DM matrix 4.24 p92
 
-                #for i in range(2):
-                #    for j in range(2):
-                #Dvp comes from N, which comes from 
-                #Dvp is [x y z yz]
-                DM[0,0]=DM[2,2]=Dvp[0]
-                DM[1,1]=DM[3,3]=Dvp[1]
-                DM[1,0]=DM[3,2]=DM[0,1]=DM[2,3]=Dvp[3]
-                DM[3,3]=Dvp[2]
-                
                 wJ=w*detJ
                 
              
@@ -486,8 +345,6 @@ while (it < numit):
                
                 
                 R[0]   =(NsigF+temp4x16*tau).transpose()*(temp4x16*UF-LM*NsigF*UF)*wJ
-                if numvars == 2:
-                    R[1]    =(Ns+tau*v.transpose()*Bs).transpose()*(v.transpose()*Bs*Us-g_sigs)*wJ
               
                 #R Assembly            
                 #TANGENT MATRIX   
@@ -502,31 +359,9 @@ while (it < numit):
                                 (temp4x16-LM*NsigF)
                                 )*wJ
                                
-                #print("temp4x16-LM*NsigF",temp4x16-LM*NsigF)         
-                #print("temp4x16",temp4x16)                          
-                #dRFdUF=dRFdUF=0.  4.37 & 4.38                
-                            
-                #---------------------------------------------------
-                #S derivatives -- COMMON TO BOTH FORMULATIONS
-                #2.53 and 4.xx ATENTION IN CHAPTER 4 k and p indices are wrong, see 2.53
-                #FORMER 3,1 and 3,3
-
-                #4.44 dRs/dF 4.44 (4x16)
-                if numvars == 2 :
-                    #print("Kt(1,0)",Kt[1][0])
-                    Kt[1][0]=Kt[1][0]+(
-                              (Ns+tau*v.transpose()*Bs).transpose()*
-                              (-dgdU[1])*
-                              wJ) 
-                    
-                    #dRs/dS 4.46    
-                    Kt[1][1]=Kt[1][1]+(
-                              (Ns+tau*v.transpose()*Bs).transpose()*
-                              (v.transpose()*Bs-dgdU[3])*
-                              wJ) 
         
         #print ("Nv",Nv)
-        #print("Kt[0][0]",Kt[0][0]) 
+        print("Kt[0][0]",Kt[0][0]) 
         
         vrowinc=0
         #Assembly Matrix
@@ -583,7 +418,7 @@ while (it < numit):
         #Deformation gradient F
         for i in range ( var_dim [ 0 ] ):
             idof = var_dim[0] * inode + i
-            #print ("idof",idof)
+            print ("idof",idof)
             for j in range(dof):
                 Kglob[ idof , j ] = 0
                 #Rglob[ j ] -= Kglob[j,idof] * 0 #dU=0, U=1(idof)
@@ -592,25 +427,15 @@ while (it < numit):
 
             Kglob[idof,idof] = 1
             Rglob[idof  ] = 0           #F INCREMENT (dF) IS NULL!!!!!
-        
-        #Sigma is zero, Internal variable s ,      
-        if numvars == 2:
-            idofs = idof + var_dim[0]
-            for i in range(dof):                    
-                Kglob[ idofs , i ] = 0
-                #Rglob[ i ] -= Kglob[ i, idofs ] * 0 #1 is R(idof)  
-                Kglob[ i , idofs ] = 0                
-        
-            Kglob[idofs,idofs] = 1
-            Rglob[idofs  ] = 0                
+                      
 
-    #print("KGLOB\n")
-    # for i in range (dof):
-        # for j in range (dof):
-            # print(Kglob[i,j], end = " ")
+    print("KGLOB\n")
+    for i in range (dof):
+        for j in range (dof):
+            print(Kglob[i,j], end = " ")
             
-        # print("\n")
-    #print("Rglob",Rglob)
+        print("\n")
+    print("Rglob",Rglob)
 
 
 #print (K)
@@ -623,20 +448,20 @@ while (it < numit):
     
     
     #TOTAL BOUNDARY CONDITIONS FOR UF and s calculations
-    # dnode=(nex+1)    
-    # for dy in range(ney+1): 
-        # inode=dy*dnode
-        # #Deformation gradient F
-        # idof = var_dim[0] * inode 
-        # print ("inode, idof",inode, idof)
-        # Uglob[ idof     ] = Uglob[ idof + 3 ] = 1
-        # Uglob[ idof + 1 ] = Uglob[ idof + 2 ] = 0
+    dnode=(nex+1)    
+    for dy in range(ney+1): 
+        inode=dy*dnode
+        #Deformation gradient F
+        idof = var_dim[0] * inode 
+        print ("inode, idof",inode, idof)
+        Uglob[ idof     ] = Uglob[ idof + 3 ] = 1
+        Uglob[ idof + 1 ] = Uglob[ idof + 2 ] = 0
 
         
-        # #Sigma is zero, Internal variable s ,      
-        # if numvars == 2:
-            # idofs = idof + var_dim[0]
-            # Uglob[ idofs ] = mat_s0
+        #Sigma is zero, Internal variable s ,      
+        if numvars == 2:
+            idofs = idof + var_dim[0]
+            Uglob[ idofs ] = mat_s0
  
     
     it+=1
